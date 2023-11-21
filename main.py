6b2065361d8bf55ef7c565169fc2b9ae2b6a8797
Makar_Popkov <zadi.super.99@mail.ru>
2023-11-21 15:54:44 +0300

    Git and random circles

--- main.py
+++ main.py
@@ -8,46 +8,13 @@ from PyQt5 import uic
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.Qt import QPointF
 
-desing = """<?xml version="1.0" encoding="UTF-8"?>
-<ui version="4.0">
- <class>Form</class>
- <widget class="QWidget" name="Form">
-  <property name="geometry">
-   <rect>
-    <x>0</x>
-    <y>0</y>
-    <width>471</width>
-    <height>546</height>
-   </rect>
-  </property>
-  <property name="windowTitle">
-   <string>Form</string>
-  </property>
-  <widget class="QPushButton" name="pushButton">
-   <property name="geometry">
-    <rect>
-     <x>140</x>
-     <y>440</y>
-     <width>171</width>
-     <height>61</height>
-    </rect>
-   </property>
-   <property name="text">
-    <string>СГЕНЕРИРОВАТЬ</string>
-   </property>
-  </widget>
- </widget>
- <resources/>
- <connections/>
-</ui>
-"""
+from UI import Ui_Form
 
 
-class Circle(QWidget):
+class Circle(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
-        f = io.StringIO(desing)
-        uic.loadUi(f, self)
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.draw)
         self.flag = False
 
@@ -63,10 +30,10 @@ class Circle(QWidget):
 
     def draw_circle(self, qp):
         if self.flag:
-            qp.setBrush(QColor(255, 255, 0))
+            qp.setBrush(QColor(randrange(0, 256), randrange(0, 256),
+                               randrange(0, 256)))
             size = randrange(30, 200)
             qp.drawEllipse(QPointF(215, 200), size, size)
-
 
 
 if __name__ == '__main__':
